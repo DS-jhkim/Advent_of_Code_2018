9b1ec89f7e880f29f175548e5a3aabb13d330c28
DS-jhkim <60243182+DS-jhkim@users.noreply.github.com>
2021-07-03 11:30:37 +0300

    Add files via upload

---
+++ day_2.py
@@ -0,0 +1,49 @@
+import fileinput
+from collections import Counter
+
+lines = [x.strip() for x in fileinput.input(files = 'input.txt')] 
+
+def part_1():
+    two_counter = 0 
+    three_counter = 0 
+    for line in lines:
+        # Counter returns a dictionary containing keys and counts
+        # values() returns a list of all the values available in a given dictionary.
+        charCounter = Counter(line).values()
+        if 2 in charCounter:
+            two_counter += 1
+        if 3 in charCounter:
+            three_counter += 1
+    print(two_counter * three_counter)
+    
+def part_2():
+    # Collects the commonString and its appearance count
+    dict = {} 
+    
+    # Need to check all other lines for each line: Nested Loops 
+    for line1 in lines:
+        diffSum = 0
+        commonStr = ""
+        for line2 in lines:
+            diffCounter = 0
+            for i in range(len(line1)):
+                if line1[i] != line2[i]:
+                    diffCounter += 1
+            # Check two strings are differend by a single character
+            if diffCounter == 1:
+                diffSum += 1
+                for i in range(len(line1)):
+                    if line1[i] != line2[i]:
+                        # To avoid repetition of assigning same string
+                        # This can be improved to increase efficiency
+                        if not commonStr:
+                            commonStr = line1[0 : i : ] + line1[i+1 : : ]
+        # Check if commonStr is not empty
+        if commonStr:
+            dict[commonStr] = diffSum
+        
+    # Print the common string with the highest appearance
+    print(max(dict, key = dict.get))
+            
+part_1()     
+part_2()
